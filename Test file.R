aa8afc91564501ca92a2080c0db4808c57123d6f
Corbett <master78east@yahoo.com>
2019-02-25 17:13:48 -0700

    SVM Model

--- Test file.R
+++ Test file.R
@@ -1,9 +1,55 @@
-a <- matrix(c(1,2,3,4,7,5,3,6,9) , nrow = 3 , ncol = 3 , byrow = TRUE)
-print(a)
-det(a)
-b <- matrix(c(1,1,4,8,11,-2,0,4,7) , nrow = 3 , ncol = 3 , byrow = TRUE)
-print(b)
-det(b)
-c <- matrix(c(2,7,0,1,5,6,4,8,0,0,9,0,1,-3,1,4) , nrow = 4 , ncol = 4 , byrow = TRUE)
-print(c)
-det(c)
+library(readr)
+shelter_skim <- read_csv("Econ4670researchproject/shelter_skim.csv")
+
+#Function that returns Root Mean Squared Error
+rmse <- function(error)
+{
+  sqrt(mean(error^2))
+}
+
+#Import Library
+library(e1071) #Contains the SVM 
+Train <- shelter_skim[1:742,]
+Test <- shelter_skim[743:1060,]
+# there are various options associated with SVM training; like changing kernel, gamma and C value.
+
+#model linear
+model_l <- svm(StayedInShelterMoreThanOnce ~.,data=Train, kernel = "linear", type = "C-classification", gamma=0.2, cost=100)
+
+#Predict Output
+preds_l <- predict(model_l,Test)
+table(preds_l, Test$StayedInShelterMoreThanOnce)
+
+#rmse linear
+actual <- as.integer(Test$StayedInShelterMoreThanOnce)
+preds_sl <- as.integer(preds_l)
+error_l <- preds_sl - actual
+rmse(error_l)
+
+#model radial
+model_r <- svm(StayedInShelterMoreThanOnce ~.,data=Train, kernel = "radial", type = "C-classification", gamma=0.2, cost=100)
+
+#Predict Output
+preds_r <- predict(model_r,Test)
+table(preds_r, Test$StayedInShelterMoreThanOnce)
+
+#rmse radial
+actual <- as.integer(Test$StayedInShelterMoreThanOnce)
+preds_sr <- as.integer(preds_r)
+error_r <- preds_sr - actual
+rmse(error_r)
+
+#model polynomial
+model_p <- svm(StayedInShelterMoreThanOnce ~.,data=Train, kernel = "polynomial", type = "C-classification", gamma=0.2, cost=100)
+
+#Predict Output
+preds_p <- predict(model_p,Test)
+table(preds_p, Test$StayedInShelterMoreThanOnce)
+
+#rmse linear
+actual <- as.integer(Test$StayedInShelterMoreThanOnce)
+preds_sp <- as.integer(preds_p)
+error_p <- preds_sp - actual
+rmse(error_p)
+
+summary(model_l)
